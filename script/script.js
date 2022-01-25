a473572bbeb083d235e7698fa79f414faffad442
luwat <oluwatobioreniyi@gmail.com>
2022-01-25 07:27:03 +0100

    fresh commit

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 //interact with player
 //creating a prompt
 let name = prompt("What's your name");
-console.log("Hello " + name);
+alert("Hello " + name);
 
 //Using corfirm to ask some yes/no questions
 var likeGame = confirm("Do you like hangman game?");
 if (likeGame) {
-    console.log("Very Good!")
+    alert("Very Good!")
 } else {
-    console.log("Please feel free to play, You'll enjoy it!");
+    alert("Please feel free to play, You'll enjoy it!");
 }
